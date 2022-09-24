bd5e8ba964d6fc4e4641b5e0f232be32d5955e51
William Oldham <wegg7250@gmail.com>
2022-09-24 22:55:29 +0100

    Add logging, enabled incremental compilation and add jda-ktx

--- src/main/kotlin/org/cascadebot/bot/Main.kt
+++ src/main/kotlin/org/cascadebot/bot/Main.kt
@@ -1,7 +1,12 @@
 @file:JvmName("Main")
 package org.cascadebot.bot
 
+import dev.minn.jda.ktx.util.SLF4J
+
+val logger by SLF4J("Main")
+
 fun main() {
     println("Hello World!")
     println(config)
+    logger.info("WOO")
 }
