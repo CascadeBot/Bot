3ede090608e3e8f726fa4557157fd19373142e3a
William Oldham <wegg7250@gmail.com>
2022-09-25 10:25:33 +0100

    Main: Convert main to object and move some bits into functions. Also add global exception handling

--- src/main/kotlin/org/cascadebot/bot/Main.kt
+++ src/main/kotlin/org/cascadebot/bot/Main.kt
@@ -7,42 +7,66 @@ import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder
 import net.dv8tion.jda.api.sharding.ShardManager
 import org.cascadebot.bot.db.PostgresManager
 import org.cascadebot.bot.events.ReadyListener
+import kotlin.system.exitProcess
 
-class Main {
-
+object Main {
     val logger by SLF4J("Main")
-    var shardManager: ShardManager
-    var postgresManager: PostgresManager
-    init {
+
+    lateinit var shardManager: ShardManager
+        private set
+    lateinit var postgresManager: PostgresManager
+        private set
+    lateinit var config: Config
+        private set
+
+    private fun runBot() {
         logger.info("Starting CascadeBot")
 
-        val config = readConfig()
+        config = loadConfig()
 
+        shardManager = buildShardManager()
+
+        postgresManager = PostgresManager(config.database)
+    }
+
+    private fun loadConfig(): Config {
+        val configResult = Config.load()
+        if (configResult.isInvalid()) {
+            // Print out the invalid config message. Replace all double new lines with single for compactness
+            logger.error("Could not load config: \n" + configResult.getInvalidUnsafe().description().replace("\n\n", "\n"))
+            exitProcess(1)
+        }
+
+        return configResult.getUnsafe()
+    }
+
+    private fun buildShardManager(): ShardManager {
         val defaultShardManagerBuilder = DefaultShardManagerBuilder.create(GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS)) // TODO do we want to have all here? I imagine eventually, but don't know about mvp
             .setToken(config.botConfig.token)
             .setActivityProvider { Activity.playing("Cascade Bot") }
             .addEventListeners(ReadyListener(this))
 
-        val shardConfig: Sharding? = config.botConfig.shards
-        if (shardConfig != null) {
-            when (shardConfig) {
+        config.botConfig.shards?.let {
+            when (it) {
                 is Sharding.Total -> {
-                    defaultShardManagerBuilder.setShardsTotal(shardConfig.total)
+                    defaultShardManagerBuilder.setShardsTotal(it.total)
                 }
                 is Sharding.MinMax -> {
-                    defaultShardManagerBuilder.setShardsTotal(shardConfig.total)
-                    defaultShardManagerBuilder.setShards(shardConfig.min, shardConfig.max)
+                    defaultShardManagerBuilder.setShardsTotal(it.total)
+                    defaultShardManagerBuilder.setShards(it.min, it.max)
                 }
             }
         }
 
-        shardManager = defaultShardManagerBuilder.build()
-
-        postgresManager = PostgresManager(config.database)
-
+        return defaultShardManagerBuilder.build()
     }
-}
 
-fun main() {
-    Main()
+    @JvmStatic
+    fun main(args: Array<String>) {
+        try {
+            runBot()
+        } catch (e: Exception) {
+            logger.error("Error in bot execution", e)
+        }
+    }
 }
