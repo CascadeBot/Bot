bf987ca520f70153933aa57bdbb7400ec8668bfd
William Oldham <wegg7250@gmail.com>
2022-10-14 13:33:33 +0100

    Whoops, spelling

--- src/main/kotlin/org/cascadebot/bot/db/PostgresManager.kt
+++ src/main/kotlin/org/cascadebot/bot/db/PostgresManager.kt
@@ -41,7 +41,7 @@ class PostgresManager(config: Database) {
         var username = config.username
         var password = config.password?.value
 
-        // This section allows the user of username and password in the URL
+        // This section allows the use of username and password in the URL
         // Hikari will not accept a URL with the userinfo so we parse it out, remove it from the url
         // and pass it in separately
         val matches = urlCredentialsRegex.find(url)
