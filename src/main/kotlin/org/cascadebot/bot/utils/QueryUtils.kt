40a37e61193e6a718be4867d241e804e0a288b18
William Oldham <wegg7250@gmail.com>
2022-11-11 20:53:27 +0000

    QueryUtils: Add Entitiy deletion helper

--- src/main/kotlin/org/cascadebot/bot/utils/QueryUtils.kt
+++ src/main/kotlin/org/cascadebot/bot/utils/QueryUtils.kt
@@ -1,9 +1,8 @@
 package org.cascadebot.bot.utils
 
 import jakarta.persistence.criteria.Predicate
-import org.cascadebot.bot.db.entities.CustomCommandEntity
-import org.cascadebot.bot.db.entities.GuildSlotEntity
 import org.hibernate.Session
+import org.hibernate.query.MutationQuery
 import org.hibernate.query.Query
 import org.hibernate.query.criteria.HibernateCriteriaBuilder
 import org.hibernate.query.criteria.JpaEntityJoin
@@ -12,30 +11,34 @@ import org.hibernate.query.criteria.JpaRoot
 object QueryUtils {
 
     fun <T> Session.getEntities(clazz: Class<T>) : Query<T> {
-        val builder = criteriaBuilder
-        val query = builder.createQuery(clazz)
+        val query = criteriaBuilder.createQuery(clazz)
         query.select(query.from(clazz))
 
         return createQuery(query)
     }
 
     fun <T> Session.queryEntity(clazz: Class<T>, whereClause: HibernateCriteriaBuilder.(JpaRoot<T>)->Predicate) : Query<T> {
-        val builder = criteriaBuilder
-        val query = builder.createQuery(clazz)
+        val query = criteriaBuilder.createQuery(clazz)
         val root = query.from(clazz)
-        query.select(root).where(whereClause(builder, root))
+        query.select(root).where(whereClause(criteriaBuilder, root))
 
         return createQuery(query)
     }
 
     fun <T, J> Session.queryJoinedEntities(clazz: Class<T>, joiningClazz: Class<J>, whereClause: HibernateCriteriaBuilder.(JpaRoot<T>, JpaEntityJoin<J>) -> Predicate): Query<T> {
-        val builder = criteriaBuilder
-        val query = builder.createQuery(clazz)
+        val query = criteriaBuilder.createQuery(clazz)
         val root = query.from(clazz)
         val join = root.join(joiningClazz)
-        query.where(whereClause(builder, root, join))
+        query.where(whereClause(criteriaBuilder, root, join))
 
         return createQuery(query)
+    }
+
+    fun <T> Session.deleteEntity(clazz: Class<T>, whereClause: HibernateCriteriaBuilder.(JpaRoot<T>)->Predicate): Int {
+        val builder = criteriaBuilder.createCriteriaDelete(clazz)
+        builder.where(whereClause(criteriaBuilder, builder.target))
+
+        return createMutationQuery(builder).executeUpdate()
     }
 
 }
