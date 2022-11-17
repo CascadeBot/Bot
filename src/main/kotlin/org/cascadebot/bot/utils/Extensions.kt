7bfb0a91b2eef9d6f4f3ae3809b7eacfa89303c3
William Oldham <wegg7250@gmail.com>
2022-11-17 23:03:32 +0000

    Extensions.kt: Comments

--- src/main/kotlin/org/cascadebot/bot/utils/Extensions.kt
+++ src/main/kotlin/org/cascadebot/bot/utils/Extensions.kt
@@ -6,6 +6,13 @@ import net.dv8tion.jda.api.entities.emoji.CustomEmoji
 import net.dv8tion.jda.api.entities.emoji.Emoji
 import net.dv8tion.jda.api.entities.emoji.UnicodeEmoji
 
+/**
+ * Returns a representation of an emoji depending on the type:
+ * - For [Emoji.Type.UNICODE], the Unicode character from [UnicodeEmoji.getName] is used.
+ * - For [Emoji.Type.CUSTOM], the emote's ID from [CustomEmoji.getId] is used.
+ *
+ * @author CascadeBot
+ */
 val Emoji.idOrName
     get() = if (this.type == Emoji.Type.UNICODE) {
         (this as UnicodeEmoji).name
@@ -13,6 +20,11 @@ val Emoji.idOrName
         (this as CustomEmoji).id
     }
 
+/**
+ * Helper function to create and configure a [SimpleModule] and then add it to the [JsonMapper.Builder].
+ *
+ * @author CascadeBot
+ */
 fun JsonMapper.Builder.addModule(builder: SimpleModule.()->Unit) {
     val mod = SimpleModule()
     builder(mod)
