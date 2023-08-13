48e6b4d40dab88f0be3547e2640ff934429035d5
William Oldham <wegg7250@gmail.com>
2023-08-13 11:00:10 +0100

    docs(query-utils): add docs for query util methods

--- src/main/kotlin/org/cascadebot/bot/utils/QueryUtils.kt
+++ src/main/kotlin/org/cascadebot/bot/utils/QueryUtils.kt
@@ -1,8 +1,8 @@
 package org.cascadebot.bot.utils
 
+import jakarta.persistence.criteria.CriteriaQuery
 import jakarta.persistence.criteria.Predicate
 import org.hibernate.Session
-import org.hibernate.query.MutationQuery
 import org.hibernate.query.Query
 import org.hibernate.query.criteria.HibernateCriteriaBuilder
 import org.hibernate.query.criteria.JpaEntityJoin
@@ -11,14 +11,30 @@ import org.hibernate.query.sqm.tree.SqmJoinType
 
 object QueryUtils {
 
-    fun <T> Session.getEntities(clazz: Class<T>) : Query<T> {
+    /**
+     * Selects all entities with the specified [clazz] with no condition. [clazz] must be a valid Hibernate table.
+     *
+     * @param clazz The class of the entity to query.
+     */
+    fun <T> Session.getEntities(clazz: Class<T>): Query<T> {
         val query = criteriaBuilder.createQuery(clazz)
         query.select(query.from(clazz))
 
         return createQuery(query)
     }
 
-    fun <T> Session.queryEntity(clazz: Class<T>, whereClause: HibernateCriteriaBuilder.(JpaRoot<T>)->Predicate) : Query<T> {
+    /**
+     * Selects entities of type [clazz] which match the provided [whereClause]. [clazz] must be a valid Hibernate table.
+     *
+     * [whereClause] has [HibernateCriteriaBuilder] as the receiver with [JpaRoot] provided as a parameter.
+     *
+     * @param clazz The class of the entity to query.
+     * @param whereClause The function which is used to construct a [Predicate] for the [CriteriaQuery.where] function.
+     */
+    fun <T> Session.queryEntity(
+        clazz: Class<T>,
+        whereClause: HibernateCriteriaBuilder.(JpaRoot<T>) -> Predicate
+    ): Query<T> {
         val query = criteriaBuilder.createQuery(clazz)
         val root = query.from(clazz)
         query.select(root).where(whereClause(criteriaBuilder, root))
@@ -26,7 +42,23 @@ object QueryUtils {
         return createQuery(query)
     }
 
-    fun <T, J> Session.queryJoinedEntities(clazz: Class<T>, joiningClazz: Class<J>, whereClause: HibernateCriteriaBuilder.(JpaRoot<T>, JpaEntityJoin<J>) -> Predicate): Query<T> {
+    /**
+     * Selects entities of type [clazz] which match the provided [whereClause]. [joiningClazz] is included in the query
+     * allowing the [whereClause] to use the joined entity's values in the query. Helpful for querying entities when
+     * related fields from another entity are needed.
+     *
+     * [whereClause] has [HibernateCriteriaBuilder] as the receiver with [JpaRoot] (The root entity) and [JpaEntityJoin]
+     * (The joined entity) provided as parameters.
+     *
+     * @param clazz The class of the entity to query.
+     * @param joiningClazz The class of the entity to join to the query.
+     * @param whereClause The function which is used to construct a [Predicate] for the [CriteriaQuery.where] function.
+     */
+    fun <T, J> Session.queryJoinedEntities(
+        clazz: Class<T>,
+        joiningClazz: Class<J>,
+        whereClause: HibernateCriteriaBuilder.(JpaRoot<T>, JpaEntityJoin<J>) -> Predicate
+    ): Query<T> {
         val query = criteriaBuilder.createQuery(clazz)
         val root = query.from(clazz)
         val join = root.join(joiningClazz, SqmJoinType.CROSS)
@@ -35,6 +67,14 @@ object QueryUtils {
         return createQuery(query)
     }
 
+    /**
+     * **Deletes** all entities of type [clazz] which match the condition by the [whereClause].
+     *
+     * [whereClause] has [HibernateCriteriaBuilder] as the receiver with [JpaRoot] provided as a parameter.
+     *
+     * @param clazz The class of the entity to delete.
+     * @param whereClause The function which is used to construct a [Predicate] for the [CriteriaQuery.where] function.
+     */
     fun <T> Session.deleteEntity(clazz: Class<T>, whereClause: HibernateCriteriaBuilder.(JpaRoot<T>)->Predicate): Int {
         val builder = criteriaBuilder.createCriteriaDelete(clazz)
         builder.where(whereClause(criteriaBuilder, builder.target))
